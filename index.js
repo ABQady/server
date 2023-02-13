3f9a6d207733893493fd8a7010db1a9f0cbe6da7
Ahmed Al Qady <26540442+ABQady@users.noreply.github.com>
2023-02-13 20:42:58 +0200

    Kos o5tak

--- index.js
+++ index.js
@@ -16,24 +16,26 @@ app.use(express.json())
 
 //app.use(cors({ origin: "https://client-abqady.vercel.app/" }));
 
-// app.use(function (req, res, next) {
+app.use(function (req, res, next) {
 
-//    // Website you wish to allow to connect
-//    res.setHeader('Access-Control-Allow-Origin', '*');
+   // Website you wish to allow to connect
+   res.setHeader('Access-Control-Allow-Origin', '*');
 
-//    // Request methods you wish to allow
-//    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+   // Request methods you wish to allow
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-//    // Request headers you wish to allow
-//    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+   // Request headers you wish to allow
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
-//    // Set to true if you need the website to include cookies in the requests sent
-//    // to the API (e.g. in case you use sessions)
-//    res.setHeader('Access-Control-Allow-Credentials', true);
+   res.setHeader('Access-Control-Allow-Credentials', true);
 
-//    // Pass to next layer of middleware
-//    next();
-// });
+   // Set to true if you need the website to include cookies in the requests sent
+   // to the API (e.g. in case you use sessions)
+   res.setHeader('Access-Control-Allow-Credentials', true);
+
+   // Pass to next layer of middleware
+   next();
+});
 
 app.get('/', (req, res) => {
    res.send('Hey this is my API running 🥳')
