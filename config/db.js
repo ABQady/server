485afcc71eb63da0c971f61bdce4a0f773d1f968
Ahmed Al Qady <26540442+ABQady@users.noreply.github.com>
2023-02-13 17:58:15 +0200

    a7aaaa

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2')
+const mysql = require('mysql')
 const db = mysql.createConnection('mysql://teqkyrb3l23iheinqp7a:pscale_pw_JLSpXzbNVZwIPgFBnTKIXFqyuELe5CtXeC3XrvXGH7G@eu-central.connect.psdb.cloud/products?ssl={"rejectUnauthorized":true}')
 
 module.exports = db;
